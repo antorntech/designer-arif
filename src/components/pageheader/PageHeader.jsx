b7d8136227f1d03b26910b6bc5ac1690440b0c56
MD Abdul Motaleb Antor <antor@piit.local>
2024-06-26 17:05:03 +0600

    add some change on pageheader section

--- src/components/pageheader/PageHeader.jsx
+++ src/components/pageheader/PageHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const PageHeader = ({ title }) => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-2 md:gap-5 h-[120px] md:h-[300px] bg-[url('pageheader.png')] bg-no-repeat bg-center bg-cover">
+    <div className="w-full flex flex-col justify-center items-center gap-2 md:gap-5 h-[120px] md:h-[300px] pageheader">
       <h1 className="text-2xl font-bold text-center text-white md:text-5xl">
         {title}
       </h1>
