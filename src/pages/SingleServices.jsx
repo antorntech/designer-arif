641144ecd189aab1f3884655736eb8897f3d42c3
antor10655gh <creativefreelancerantor@gmail.com>
2024-06-28 12:46:12 +0600

    add all services

--- src/pages/SingleServices.jsx
+++ src/pages/SingleServices.jsx
@@ -4,12 +4,42 @@ import { Link, useLocation } from "react-router-dom";
 
 const SingleServices = () => {
   const alllogodesigns = [
-    { id: 1, name: "Logo Design 1", thumbnail: "logodesign01.png", link: "/" },
-    { id: 2, name: "Logo Design 2", thumbnail: "logodesign02.png", link: "/" },
-    { id: 3, name: "Logo Design 3", thumbnail: "logodesign03.png", link: "/" },
-    { id: 4, name: "Logo Design 4", thumbnail: "logodesign04.png", link: "/" },
-    { id: 5, name: "Logo Design 5", thumbnail: "logodesign05.png", link: "/" },
-    { id: 6, name: "Logo Design 6", thumbnail: "logodesign06.png", link: "/" },
+    {
+      id: 2,
+      name: "Logo Design 1",
+      thumbnail: "/images/logodesign01.png",
+      link: "/",
+    },
+    {
+      id: 1,
+      name: "Logo Design 2",
+      thumbnail: "/images/logodesign02.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Logo Design 3",
+      thumbnail: "/images/logodesign03.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Logo Design 4",
+      thumbnail: "/images/logodesign04.png",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "Logo Design 5",
+      thumbnail: "/images/logodesign01.png",
+      link: "/",
+    },
+    {
+      id: 6,
+      name: "Logo Design 6",
+      thumbnail: "/images/logodesign02.png",
+      link: "/",
+    },
   ];
 
   const allbrandings = [
@@ -51,6 +81,162 @@ const SingleServices = () => {
     },
   ];
 
+  const allPrintDesigns = [
+    {
+      id: 1,
+      name: "Print Design 1",
+      thumbnail: "/images/printdesign01.png",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Print Design 2",
+      thumbnail: "/images/printdesign02.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Print Design 3",
+      thumbnail: "/images/printdesign03.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Print Design 4",
+      thumbnail: "/images/printdesign04.png",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "Print Design 5",
+      thumbnail: "/images/printdesign01.png",
+      link: "/",
+    },
+    {
+      id: 6,
+      name: "Print Design 6",
+      thumbnail: "/images/printdesign02.png",
+      link: "/",
+    },
+  ];
+
+  const allSocialMedia = [
+    {
+      id: 1,
+      name: "Social Media 1",
+      thumbnail: "/images/socialmedia01.png",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Social Media 2",
+      thumbnail: "/images/socialmedia02.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Social Media 3",
+      thumbnail: "/images/socialmedia03.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Social Media 4",
+      thumbnail: "/images/socialmedia04.png",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "Social Media 5",
+      thumbnail: "/images/socialmedia01.png",
+      link: "/",
+    },
+    {
+      id: 6,
+      name: "Social Media 6",
+      thumbnail: "/images/socialmedia02.png",
+      link: "/",
+    },
+  ];
+
+  const allAnimations = [
+    {
+      id: 1,
+      name: "Animation 1",
+      thumbnail: "/images/animation01.png",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Animation 2",
+      thumbnail: "/images/animation02.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Animation 3",
+      thumbnail: "/images/animation03.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Animation 4",
+      thumbnail: "/images/animation04.png",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "Animation 5",
+      thumbnail: "/images/animation01.png",
+      link: "/",
+    },
+    {
+      id: 6,
+      name: "Animation 6",
+      thumbnail: "/images/animation02.png",
+      link: "/",
+    },
+  ];
+
+  const all3dModeling = [
+    {
+      id: 1,
+      name: "3dmodeling 1",
+      thumbnail: "/images/3dmodeling01.png",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "3dmodeling 2",
+      thumbnail: "/images/3dmodeling02.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "3dmodeling 3",
+      thumbnail: "/images/3dmodeling03.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "3dmodeling 4",
+      thumbnail: "/images/3dmodeling04.png",
+      link: "/",
+    },
+    {
+      id: 5,
+      name: "3dmodeling 5",
+      thumbnail: "/images/3dmodeling01.png",
+      link: "/",
+    },
+    {
+      id: 6,
+      name: "3dmodeling 6",
+      thumbnail: "/images/3dmodeling02.png",
+      link: "/",
+    },
+  ];
+
   const location = useLocation();
 
   // Extract the pathname from the location object
@@ -63,10 +249,16 @@ const SingleServices = () => {
   let dataToRender = [];
   if (extractedPath === "all-logo-design") {
     dataToRender = alllogodesigns;
-    console.log(dataToRender);
   } else if (extractedPath === "all-branding") {
     dataToRender = allbrandings;
-    console.log(dataToRender);
+  } else if (extractedPath === "all-print-design") {
+    dataToRender = allPrintDesigns;
+  } else if (extractedPath === "all-social-media") {
+    dataToRender = allSocialMedia;
+  } else if (extractedPath === "all-animation") {
+    dataToRender = allAnimations;
+  } else if (extractedPath === "all-3d-modeling") {
+    dataToRender = all3dModeling;
   }
 
   return (
