b2134b47c404b112895b5a447fe80d7d89c43961
MD Abdul Motaleb Antor <antor@piit.local>
2024-07-03 18:33:02 +0600

    add some change

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import Lottie from "lottie-react";
-import bannerAnimation from "../../assets/banner.json";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.designerarif.com/api/v1/herocontent")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   const phoneNumber = "+8801996774893";
   const message = "Hello, I'm interested in your work!";
   const handleClick = () => {
@@ -19,11 +25,14 @@ const HeroSection = () => {
         <div className="w-full h-full grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="-mt-[45px] md:mt-0 mr-auto place-self-center lg:col-span-6">
             <h1 className="text-gradient text-2xl md:text-4xl lg:text-6xl font-bold tracking-tight py-5">
-              Visualize Your Brand with Professional Design Solutions
+              {data[0]?.title
+                ? data[0]?.title
+                : "Visualize Your Brand with Professional Design Solutions"}
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              I break down complex user experinece problems to create integritiy
-              focussed solutions that connect billions of people
+              {data[0]?.description
+                ? data[0]?.description
+                : "I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people"}
             </p>
             <div className="mt-6 md:mt-12 flex flex-col lg:flex-row items-center gap-4 md:gap-6">
               <Link
@@ -60,7 +69,7 @@ const HeroSection = () => {
           </div>
           <div className="lg:mt-0 lg:col-span-6 lg:flex">
             <img
-              src="hero-banner02.png"
+              src={`https://api.designerarif.com/${data[0]?.banner}`}
               alt="mockup"
               className="w-full object-contain"
             />
--- src/components/home/HomeTaskSection.jsx
+++ src/components/home/HomeTaskSection.jsx
@@ -1,55 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CountUp from "react-countup";
 
 const HomeTaskSection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.designerarif.com/api/v1/tasklist")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <>
       <div className="rounded-md bg-gradient-to-r from-[#5bf0f846] via-[#38f6cd5b] to-[#04fe8e5b] p-1 bg-[#22fab211] w-full h-full grid max-w-screen-xl py-8 mx-auto">
         <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-12">
-          <div className="flex items-center gap-2 text-white font-bold mx-auto">
-            <CountUp
-              end={5}
-              duration={5}
-              suffix="+"
-              className="text-xl md:text-5xl"
-            />
-            <p className="text-md md:text-2xl font-bold">
-              Years of <br /> Experience
-            </p>
-          </div>
-          <div className="flex items-center gap-2 text-white font-bold mx-auto">
-            <CountUp
-              end={50}
-              duration={5}
-              suffix="+"
-              className="text-xl md:text-5xl"
-            />
-            <p className="text-md md:text-2xl font-bold">
-              Project <br /> Completed
-            </p>
-          </div>
-          <div className="flex items-center gap-2 text-white font-bold mx-auto">
-            <CountUp
-              end={1}
-              duration={5}
-              suffix="K"
-              className="text-xl md:text-5xl"
-            />
-            <p className="text-md md:text-2xl font-bold">
-              Happy <br /> Clients
-            </p>
-          </div>
-          <div className="flex items-center gap-2 text-white font-bold mx-auto">
-            <CountUp
-              end={5}
-              duration={5}
-              suffix="+"
-              className="text-xl md:text-5xl"
-            />
-            <p className="text-md md:text-2xl font-bold">
-              Work for <br /> Company
-            </p>
-          </div>
+          {data?.map((item, index) => (
+            <div className="flex items-center gap-2 text-white font-bold mx-auto">
+              <CountUp
+                end={item?.value}
+                duration={5}
+                suffix="+"
+                className="text-xl md:text-5xl"
+              />
+              <span className="text-md md:text-2xl font-bold">
+                {item?.label}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
