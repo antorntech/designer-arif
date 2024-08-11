f7f71ac15f9f8ad01bc87c19e61687dee0d31415
AM Antor <creativefreelancerantor@gmail.com>
2024-08-11 11:56:17 +0600

    add some change

--- src/components/home/HomeSlider.jsx
+++ src/components/home/HomeSlider.jsx
@@ -3,40 +3,79 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+function SampleNextArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div onClick={onClick}>
+      <i class="fa-solid fa-angle-right text-white absolute top-1/2 right-0 bg-[#0bb0ba] p-2 cursor-pointer z-30"></i>
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div onClick={onClick}>
+      <i class="fa-solid fa-angle-left text-white absolute top-1/2 left-0 bg-[#0bb0ba] p-2 cursor-pointer z-30"></i>
+    </div>
+  );
+}
+
 const HomeSlider = () => {
   const sliders = [
     {
       id: 1,
-      banner: "images/slider1.png",
+      mobilePhoto: "images/slider5.png",
+      desktopPhoto: "images/slider1.png",
     },
     {
       id: 2,
-      banner: "images/slider3.png",
+      mobilePhoto: "images/slider5.png",
+      desktopPhoto: "images/slider3.png",
     },
     {
       id: 3,
-      banner: "images/slider1.png",
+      mobilePhoto: "images/slider5.png",
+      desktopPhoto: "images/slider1.png",
     },
   ];
 
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
-    <section className="z-[-1] my-5 md:my-12 lg:my-24 overflow-hidden">
-      <div className="slider-container mx-10">
-        <Slider {...settings}>
-          {sliders.map((slider) => (
-            <div key={slider.id} className="slider">
-              <img src={slider.banner} alt="" className="w-full h-full" />
-            </div>
-          ))}
-        </Slider>
+    <section className="z-[-1] my-5 md:my-12 lg:my-28  overflow-hidden">
+      <div className="w-full h-full max-w-screen-xl mx-auto px-4 py-4">
+        <div className="slider-container">
+          <Slider {...settings}>
+            {sliders.map((slider) => (
+              <>
+                <div key={slider.id} className="slider hidden md:block">
+                  <img
+                    src={slider.desktopPhoto}
+                    alt=""
+                    className="w-full h-full"
+                  />
+                </div>
+                <div key={slider.id} className="slider block md:hidden">
+                  <img
+                    src={slider.mobilePhoto}
+                    alt=""
+                    className="w-[85%] h-full"
+                  />
+                </div>
+              </>
+            ))}
+          </Slider>
+        </div>
       </div>
     </section>
   );
